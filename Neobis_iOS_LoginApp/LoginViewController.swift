eedc4da84cee24a1f066e3706e411b1c2114ed53
iPakTulane <ipak.tulane@gmail.com>
2023-10-20 14:26:19 +0500

    Change ViewController name to LoginViewController

--- Neobis_iOS_LoginApp/LoginViewController.swift
+++ Neobis_iOS_LoginApp/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Neobis_iOS_LoginApp
 //
 //  Created by Igor Pak on 17/10/23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
